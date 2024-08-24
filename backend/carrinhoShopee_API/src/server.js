7c5169e45c1a8f6f051a794df924f128e490b165
yyhago <yhago.felipe.teles@gmail.com>
2024-08-23 22:09:30 -0300

    Project Finished!

--- backend/carrinhoShopee_API/src/server.js
+++ backend/carrinhoShopee_API/src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}!`);
 });
